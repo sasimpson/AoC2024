9300159e27fbc86f53637c09d90544593e2b18d6
Scott Simpson <sasimpson@gmail.com>
2024-12-02 21:39:45 -0600

    solution day 1b

--- cmd/day1/main.go
+++ cmd/day1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"slices"
@@ -11,17 +12,49 @@ import (
 )
 
 func main() {
-	var a1, a2 []int
-	var sum int
-
+	fmt.Println("day 1")
 	fp, err := os.Open("cmd/day1/data1a.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fp.Close()
 
-	scanner := bufio.NewScanner(fp)
+	a1, a2 := makeLists(fp)
 
+	fmt.Println("part 1 the sum is", partOne(a1, a2))
+	fmt.Println("part 2 the sum is", partTwo(a1, a2))
+}
+
+func partOne(a1, a2 []int) int {
+	var sum int
+
+	slices.Sort(a1)
+	slices.Sort(a2)
+
+	for i := 0; i < len(a1); i++ {
+		sum = sum + absInt(a1[i], a2[i])
+	}
+	return sum
+}
+
+func partTwo(a1, a2 []int) int {
+	var sum int
+	for _, left := range a1 {
+		var count int
+		for _, right := range a2 {
+			if left == right {
+				count++ // the left number exists in the right, so count it
+			}
+		}
+		sum = sum + (count * left)
+	}
+	return sum
+}
+
+func makeLists(file io.Reader) ([]int, []int) {
+	var a1, a2 []int
+
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), "   ")
 		if len(line) == 2 {
@@ -35,15 +68,7 @@ func main() {
 			}
 		}
 	}
-
-	slices.Sort(a1)
-	slices.Sort(a2)
-
-	for i := 0; i < len(a1); i++ {
-		sum = sum + absInt(a1[i], a2[i])
-	}
-
-	fmt.Println("the sum is", sum)
+	return a1, a2
 }
 
 func absInt(a, b int) int {
