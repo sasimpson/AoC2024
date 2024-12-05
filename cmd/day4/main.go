46dd931114992086796dda08fa839c26ecea5600
Scott Simpson <sasimpson@gmail.com>
2024-12-04 22:18:46 -0600

    data load setup

--- cmd/day4/main.go
+++ cmd/day4/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+)
 
+func main() {
+	fp, err := os.Open("cmd/day4/data4.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+	var lines []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
 }
