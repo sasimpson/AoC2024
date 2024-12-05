4fc229c6ffebd34d69e1c93c5d34f5ce6937bd1d
Scott Simpson <sasimpson@gmail.com>
2024-12-04 20:58:20 -0600

    day 3 part 1 complete

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+var (
+	mulRegExp = regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+)
+
 func main() {
+	fp, err := os.Open("cmd/day3/data3.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fp.Close()
+
+	var sum int64
+	scanner := bufio.NewScanner(fp)
+	for scanner.Scan() {
+		data := scanner.Bytes()
+
+		matches := mulRegExp.FindAllSubmatch(data, -1)
+		for _, match := range matches {
+			a, _ := strconv.ParseInt(string(match[1]), 10, 64)
+			b, _ := strconv.ParseInt(string(match[2]), 10, 64)
+			prod := a * b
+			sum = sum + prod
+		}
+	}
+	fmt.Println("day 3 sum: ", sum)
 
 }
