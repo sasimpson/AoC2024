c3be7c61805618b708ce8767a3188ff4724b560f
Scott Simpson <sasimpson@gmail.com>
2024-12-07 15:52:58 -0600

    day 4 part 1 complete working on part 2

--- cmd/day4/main.go
+++ cmd/day4/main.go
@@ -23,32 +23,178 @@ func main() {
 		data = append(data, splitLine)
 	}
 
-	//patterns for search:
-	/*
-		XMAS 	data[i][j:j+4]
-		SAMX
-
-		X S		data[i:i+4][j]
-		M A
-		A M
-		S X
-
-		X S		data[i:i+4][j:j+4]
-		 M A
-		  A M
-		   S X
-
-		   S X	data[i:i+4][j+4:j]
-		  A M
-		 M A
-		X S
-	*/
-
-	fmt.Println(data)
+	part1(data)
+	//part2(data)
 }
 
-func searchForward(data [][]string, search string, x, y int) bool {
-	s := data[y][x : x+4]
-	fmt.Println(s)
+func part1(data [][]string) {
+	var count int
+	for y := 0; y < len(data); y++ {
+		for x := 0; x < len(data[0]); x++ {
+			if data[y][x] == "X" {
+				count += searchPosition(data, "XMAS", x, y)
+			}
+		}
+	}
+	fmt.Println("day 4 part 1", count)
+}
+
+func part2(data [][]string) {
+	var count int
+	for y := 0; y < len(data); y++ {
+		for x := 0; x < len(data[0]); x++ {
+			if data[y][x] == "A" {
+				fmt.Println("found an A at", x, y)
+				count = 0
+			}
+		}
+	}
+	fmt.Println("day 4 part 2", count)
+}
+
+func LRMas(data [][]string, x, y int, dir int) bool {
+	//  012
+	//0 M S
+	//1  A
+	//2 M S
+	//A's (1 <= x pos < len(data[y]) and (1 <= y pos < len(data))
+	//M's in (x-1,y-1) and (x-1,y+1)
+	var count int
+	if (x >= 1 && x < len(data[y])-2) && (y >= 1 && y < len(data)-2) {
+		if searchDiagonalDown(data, "MAS", x-1, y-1, dir) &&
+			searchDiagonalUp(data, "MAS", x-1, y+1, dir) {
+			count++
+		}
+	}
+	return count > 0
+}
+
+func searchPosition(data [][]string, search string, x, y int) int {
+	var count int
+	if searchHorizontal(data, search, x, y, 1) {
+		fmt.Println("found H+ at", y, x)
+		count++
+	}
+	if searchHorizontal(data, search, x, y, -1) {
+		fmt.Println("found H- at", y, x)
+		count++
+	}
+	if searchVertical(data, search, x, y, 1) {
+		fmt.Println("found V+ at", y, x)
+		count++
+	}
+	if searchVertical(data, search, x, y, -1) {
+		fmt.Println("found V- at", y, x)
+		count++
+	}
+	if searchDiagonalDown(data, search, x, y, 1) {
+		fmt.Println("found DD+ at", y, x)
+		count++
+	}
+	if searchDiagonalDown(data, search, x, y, -1) {
+		fmt.Println("found DD- at", y, x)
+		count++
+	}
+	if searchDiagonalUp(data, search, x, y, 1) {
+		fmt.Println("found UD+ at", y, x)
+		count++
+	}
+	if searchDiagonalUp(data, search, x, y, -1) {
+		fmt.Println("found UD- at", y, x)
+		count++
+	}
+	return count
+}
+
+func searchHorizontal(data [][]string, search string, x, y, dir int) bool {
+	searchLen := len(search)
+	s := make([]string, searchLen)
+	//if going forward, check that we don't run over the end
+	if dir >= 1 {
+		if len(data[y]) > x+(searchLen-1) {
+			for i := 0; i < searchLen; i++ {
+				s[i] = data[y][x+i]
+			}
+		}
+	} else {
+		if x >= searchLen-1 {
+			for i := 0; i < searchLen; i++ {
+				s[i] = data[y][x-i]
+			}
+		}
+	}
+	toMatch := strings.Join(s, "")
+	if toMatch == search {
+		return true
+	}
+	return false
+}
+
+func searchVertical(data [][]string, search string, x, y, dir int) bool {
+	searchLen := len(search)
+	s := make([]string, searchLen)
+	if dir >= 1 {
+		if len(data) > y+(searchLen-1) {
+			for i := 0; i < searchLen; i++ {
+				s[i] = data[y+i][x]
+			}
+		}
+	} else {
+		if y >= (searchLen - 1) {
+			for i := 0; i < searchLen; i++ {
+				s[i] = data[y-i][x]
+			}
+		}
+	}
+	toMatch := strings.Join(s, "")
+	if toMatch == search {
+		return true
+	}
+	return false
+}
+
+func searchDiagonalDown(data [][]string, search string, x, y, dir int) bool {
+	searchLen := len(search)
+	s := make([]string, searchLen)
+	if dir >= 1 {
+		if len(data[y]) > x+(searchLen-1) && len(data) > y+(searchLen-1) {
+			for i := 0; i < searchLen; i++ {
+				s[i] = data[y+i][x+i]
+			}
+		}
+	} else {
+		if x >= searchLen-1 && y >= searchLen-1 {
+			for i := 0; i < searchLen; i++ {
+				s[i] = data[y-i][x-i]
+			}
+		}
+	}
+	toMatch := strings.Join(s, "")
+	if toMatch == search {
+		return true
+	}
+	return false
+}
+
+func searchDiagonalUp(data [][]string, search string, x, y, dir int) bool {
+	searchLen := len(search)
+	s := make([]string, searchLen)
+	if dir >= 1 {
+		if len(data[y]) > x+(searchLen-1) && y >= searchLen-1 {
+			for i := 0; i < searchLen; i++ {
+				s[i] = data[y-i][x+i]
+			}
+		}
+	} else {
+		if x >= searchLen-1 && len(data) > y+(searchLen-1) {
+			for i := 0; i < searchLen; i++ {
+				s[i] = data[y+i][x-i]
+			}
+		}
+	}
+	toMatch := strings.Join(s, "")
+	if toMatch == search {
+		return true
+	}
 	return false
 }
--- cmd/day4/main_test.go
+++ cmd/day4/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
 )
@@ -18,27 +18,55 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX`
 
-func Test_searchForward(t *testing.T) {
+//var searchBlob = `S..S..S
+//.A.A.A.
+//..MMM..
+//SAMXMAS
+//..MMM..
+//.A.A.A.
+//S..S..S`
+
+func Test_searchPosition(t *testing.T) {
 	scanner := bufio.NewScanner(strings.NewReader(searchBlob))
 	var data [][]string
 	for scanner.Scan() {
 		line := strings.Split(scanner.Text(), "")
 		data = append(data, line)
 	}
-
+	var count int
 	for y := 0; y < len(data); y++ {
+		//fmt.Println("Checking line", y)
 		for x := 0; x < len(data[y]); x++ {
+			//fmt.Println("Checking position", y, x, data[y][x])
 			if data[y][x] == "X" {
-				if len(data[y]) > x+4 {
-					if searchForward(data, "XMAS", x, y) {
-						fmt.Println("found")
-					}
-					if searchForward(data, "SAMX", x, y) {
-						fmt.Println("found")
-					}
+				count += searchPosition(data, "XMAS", x, y)
+			}
+		}
+	}
+	assert.Equal(t, 18, count)
+}
+
+func Test_searchMas(t *testing.T) {
+	scanner := bufio.NewScanner(strings.NewReader(searchBlob))
+	var data [][]string
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "")
+		data = append(data, line)
+	}
+	var count int
+	for y := 0; y < len(data); y++ {
+		//fmt.Println("Checking line", y)
+		for x := 0; x < len(data[y]); x++ {
+			if data[y][x] == "A" {
+				//fmt.Println("found A at", x, y)
+				if LRMas(data, x, y, 1) {
+					count++
+				}
+				if LRMas(data, x, y, -1) {
+					count++
 				}
 			}
 		}
 	}
-
+	assert.Equal(t, 9, count)
 }
